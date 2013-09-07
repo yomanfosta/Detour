540ed2081a7728c89d94e91f87b41a81ef4b0457
Josh Foster <fosterjosh@gmail.com>
2013-09-07 03:17:45 -0400

    got basic routes to work with test()

--- maps.js
+++ maps.js
@@ -1,1 +1,59 @@
-var test = {}
+var directionsDisplay;
+var directionsService = new google.maps.DirectionsService();
+var map;
+
+function init(){
+    console.log("Init!");
+    directionsDisplay = new google.maps.DirectionsRenderer();
+    var mapOptions = 
+    {
+    zoom: 8,
+    center: new google.maps.LatLng((39.952335), -75.163789),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+	}
+    map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+    directionsDisplay.setMap(map);
+    test();
+}
+function getdirections()
+{
+	
+	var start = getElementById("start-loc").value;
+	var end = getElementById("end-loc").value;
+		
+	var request = 
+	{
+		origin: start,
+		destination: end,
+		travelMode: google.maps.DirectionsTravelMode.DRIVING
+	};
+	directionsService.route(request, function(response,status) 
+		{
+			if (status == google.maps.DirectionsStatus.OK) 
+			{
+			  directionsDisplay.setDirections(response);
+			}	
+		});
+}
+
+function test()
+{
+	var start = "Philadelphia, PA";
+	var end = "Chicago, IL";
+	var request = 
+	{
+		origin: start,
+		destination: end,
+		travelMode: google.maps.DirectionsTravelMode.DRIVING
+	};
+	directionsService.route(request, function(response,status) 
+		{
+			if (status == google.maps.DirectionsStatus.OK) 
+			{
+			  directionsDisplay.setDirections(response);
+			}	
+		});
+	
+}
+
+google.maps.event.addDomListener(window, 'load', init);
