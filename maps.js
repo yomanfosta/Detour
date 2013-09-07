1fe989a37f844943d2dff46cfe7ff1c95aad55db
Josh Foster <fosterjosh@gmail.com>
2013-09-07 08:33:09 -0400

    revert

--- maps.js
+++ maps.js
@@ -1,9 +1,6 @@
 var directionsDisplay;
 var directionsService = new google.maps.DirectionsService();
 var map;
-var mapElement; 
-var detours;
-var start, end;
 
 function init(){
     console.log("Init!");
@@ -14,74 +11,29 @@ function init(){
     center: new google.maps.LatLng((39.952335), -75.163789),
     mapTypeId: google.maps.MapTypeId.ROADMAP
 	}
-	mapElement = document.getElementById("map-canvas");
-    map = new google.maps.Map(mapElement, mapOptions);
+    map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
     directionsDisplay.setMap(map);
-    detours = new Array();
-    start = '';
-    end = '';
     test();
-    var adjust = mapElement.offsetHeight * 0.3;
-    map.panBy(0,adjust);
-				
 }
-
-
-function onChange()
-{
-	var waypts = getdirections(); //get directions will redraw the map
-	//adjust
-	var adjust = mapElement.offsetHeight * 0.3;
-    map.panBy(0,adjust);
-    
-	if(waypts.length===0)
-		return;//TODO: alert fail;
-	else
-		getDetour(waypts, document.getElementById("radius").value);
-}
-
-//called solely when start or end is changed
-function changeStartEnd()
-{
-	//check that we're not wasting our time
-	
-	if(start === getElementById("start-loc").value || end === getElementById("end-loc").value)
-		return;
-	start = getElementById("start-loc").value;
-	end = getElementById("end-loc").value;
-	detours = new Array(); //updated start and destination, delete all previous detours added to itenerary
-	getDirections();
-	
-}
-
 function getdirections()
 {
 	
-	
+	var start = getElementById("start-loc").value;
+	var end = getElementById("end-loc").value;
+		
 	var request = 
 	{
 		origin: start,
-		
 		destination: end,
 		travelMode: google.maps.DirectionsTravelMode.DRIVING
 	};
-	var waypointsStrs = new Array();
 	directionsService.route(request, function(response,status) 
 		{
-			
 			if (status == google.maps.DirectionsStatus.OK) 
 			{
-				var waypoints = response.routes[0].overview_path;
-				directionsDisplay.setDirections(response);
-				
-				for(var i =0;i<waypoints.length;i++)
-				{
-					waypointsStrs[i] = waypoints[i].ob.toString() + ',' + waypoints[i].pb.toString();
-					console.log(waypointsStrs[i]); //prints to console
-				}
-			}
+			  directionsDisplay.setDirections(response);
+			}	
 		});
-	return waypointsStrs;
 }
 
 function test()
@@ -94,35 +46,13 @@ function test()
 		destination: end,
 		travelMode: google.maps.DirectionsTravelMode.DRIVING
 	};
-	var waypoints;
-	var waypointsStrs = new Array();
 	directionsService.route(request, function(response,status) 
 		{
-			var waypoints = response.routes[0].overview_path;
 			if (status == google.maps.DirectionsStatus.OK) 
 			{
-				directionsDisplay.setDirections(response);
-				directionsDisplay.setPanel(document.getElementById("directions"));
-				for(var i =0;i<waypoints.length;i++)
-				{
-					waypointsStrs[i] = waypoints[i].ob.toString() + ',' + waypoints[i].pb.toString();
-					console.log(waypointsStrs[i]); //prints to console
-				}
-				
-			}
-			
-			
-					
+			  directionsDisplay.setDirections(response);
+			}	
 		});
-	
-}
-
-function addDetour(location)
-{
-	
-	//check location doesn't already exist in detours and that we are not greater than 8.
-	detours[detours.length] = location;
-	getdirections(); 
 	
 }
 
