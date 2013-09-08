f9287c8c9c332ff0964447c5140bb975e3797915
Una Kravets <Una21136@yahoo.com>
2013-09-08 03:01:01 -0400

    arrows changing with app

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,7 @@ window.DETOUR || (DETOUR = {});
 			$('#directions').on('click', this.pullDirections);
 			$('.icon').on('click', this.clickMenu);
 			$('.go').on('click', this.pressGo);
+			$('.header').on('click', (function(){($('.info').css('visibility','visible'))}));
 		},
 
 		pullDirections: function(){
@@ -23,10 +24,11 @@ window.DETOUR || (DETOUR = {});
 			if ($this.is('.pulled')) {
 				$this.removeClass('pulled');
 				$this.animate({ scrollTop: 0 }, "slow");
+				$('.dir-arrow').text('▲');
 			}
 			else {
 				$this.addClass('pulled');
-				$('.dir-arrow').innerHTML('hi');
+				$('.dir-arrow').text('▼');
 			}
 		},
 
@@ -41,11 +43,19 @@ window.DETOUR || (DETOUR = {});
 		},
 
 		pressGo: function() {
+			var $this = $('.go');
 			$('.info').css('visibility','hidden');
-			$('#directions').css('visibility','visible');
-			$('.sub').children().addClass('open');
-
+			$('#directions, .head-arrow').css('visibility','visible');
+			
+			if ($('.go').is('.open')) {
+				$this.removeClass('open');
+				$('.sub').children().removeClass('open');
 			}
+			else {
+				$this.addClass('open');
+				$('.sub').children().addClass('open');
+			}
+		}
 	};
 
 	DETOUR.common.init(); //lol. note to self: remember to initialize
